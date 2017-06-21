36d199495d81842223958d628cefa1ae3e42bbbf
Néstor Ibarra <nstor7@hotmail.com>
2017-06-21 16:11:53 -0500

    icono de ventas terminado

--- src/servicio/template.js
+++ src/servicio/template.js
@@ -48,7 +48,7 @@ module.exports = yo`
         <div class="serviciosImagen" style="background: url('${secciones[3].ilustracion}'); background-size: contain; background-repeat: no-repeat"></div>
       </section>
       <section class="noHomeSection">
-        <span class="serviciosImagen icon-servicios-venta"></span>
+        <span class="serviciosImagen icon-servicios-venta"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span><span class="path11"></span><span class="path12"></span><span class="path13"></span><span class="path14"></span><span class="path15"></span><span class="path16"></span><span class="path17"></span><span class="path18"></span><span class="path19"></span><span class="path20"></span></span>
         <div class="serviciosTexto">
           <h2>VENTA DE MATERIALES</h2>
           <p>
