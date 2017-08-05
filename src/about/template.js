fbdfe2482217fe05e4fad9332de43ad8349e1844
Néstor Ibarra <nstor7@hotmail.com>
2017-08-05 15:29:41 -0500

    cambios en About

--- src/about/template.js
+++ src/about/template.js
@@ -56,7 +56,7 @@ module.exports = yo`
     </section>
     <section class="aboutSegunda">
       <article class="columna">
-        <p>EA Panamá nace en la Ciudad de Panamá en el año 2014. Formada por un grupo de jóvenes emprendedores Europeos y Panameños que han apostado por ofrecer una solución a los problemas acústicos que se presentan en la Ciudad de Panamá. El principal objetivo es brindar una alta calidad en todos nuestros servicios.</p>
+        <p>EA Panamá nace en la Ciudad de Panamá en el año 2014. Formada por un grupo de jóvenes emprendedores que han apostado por ofrecer una solución a los problemas acústicos que se presentan en la Ciudad de Panamá. El principal objetivo es brindar una alta calidad en todos nuestros servicios.</p>
         <p>La corporación se compromete con el medio ambiente, preocupándose desde el origen de sus insumos hasta el impacto ambiental de la actividad empresarial.</p>
       </article>
       <article class="columna">
@@ -64,8 +64,8 @@ module.exports = yo`
         <p>Brindamos soluciones de alta calidad, contando con un personal altamente capacitado y relaciones de confianza con nuestrosproveedores. Soluciones con total confidencialidad e imparcialidad, aseguran la entera confianza de nuestros clientes.</p>
       </article>
       <article class="columna">
-        <p>La MISIÓN de EA Panamá es proveer a nuestros clientes con Servicios, Productos y Tecnologías innovadoras, a través de la investigación y desarrollo, para dar soluciones acústicas de calidad a sus necesidades; comprometidos siempre con el medio ambiente, la sociedad y el buen servicio al cliente.</p>
-        <p>Nuestra VISIÓN, ser la empresa de soluciones acústicas líder en la región, convirtiéndonos así en una referencia para el desarrollo, investigación, innovación y buen servicio.</p>
+        <p>La <b>MISIÓN</b> de EA Panamá es proveer a nuestros clientes con Servicios, Productos y Tecnologías innovadoras, a través de la investigación y desarrollo, para dar soluciones acústicas de calidad a sus necesidades; comprometidos siempre con el medio ambiente, la sociedad y el buen servicio al cliente.</p>
+        <p>Nuestra <b>VISIÓN</b>, ser la empresa de soluciones acústicas líder en la región, convirtiéndonos así en una referencia para el desarrollo, investigación, innovación y buen servicio.</p>
       </article>
     </section>
     <section class="aboutInfo">
