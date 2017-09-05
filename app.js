4600ef1ffa6abaa9d292a6fb2bf125a57c6ffd3f
Néstor Ibarra <nstor7@hotmail.com>
2017-09-05 08:50:44 -0500

    about

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get('/', function(req, res){
 
 app.get('/about', function(req, res){
  res.render('index.pug', {
-  title: 'EA Panamá - Nosotros',
+  title: 'EA Panamá - about',
   keywords: 'Panamá Acústica, Panamá ruido, Panamá aislamiento, Panamá eco, Panamá, acústica.',
   description:'Empresa Acústica con servicios únicos y de calidad en Panamá. EA Panamá fue creada en 2014.',
   image: 'http://sumergidapro.com/imagenes/logoRedes.jpg'
