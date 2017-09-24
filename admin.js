c7c97183c263a40b874292d43c1e32df60226fdd
Néstor Ibarra <nstor7@hotmail.com>
2017-09-24 01:57:08 -0500

    corrección de url

--- admin.js
+++ admin.js
@@ -5,7 +5,7 @@ const secret = require('../.secret')
 admin.use(express.static('public'))
 
 admin.get('/', function(req, res){
- res.render('index.pug', {title: 'EA Panamá - Admin', image: 'imagenes/logoRedes.jpg'})
+ res.render('index.pug', {title: 'EA Panamá - Admin', image: 'imagenes/logoRedes.jpg', url: '/admin'})
 })
 
 admin.get('/auth', function(req, res){
@@ -17,15 +17,15 @@ admin.get('/auth', function(req, res){
 })
 
 admin.get('/editor', function(req, res){
- res.render('index.pug', {title: 'EA Panamá - Editor', image: 'imagenes/logoRedes.jpg'})
+ res.render('index.pug', {title: 'EA Panamá - Editor', image: 'imagenes/logoRedes.jpg', url: '/admin/editor'})
 })
 
 admin.get('/lista', function(req, res){
- res.render('index.pug', {title: 'EA Panamá - Consola', image: 'imagenes/logoRedes.jpg'})
+ res.render('index.pug', {title: 'EA Panamá - Consola', image: 'imagenes/logoRedes.jpg', url: '/admin/lista'})
 })
 
 admin.get('/editor/:titulo', function(req, res){
- res.render('index.pug', {title: 'EA Panamá - Editor', image: 'imagenes/logoRedes.jpg'})
+ res.render('index.pug', {title: 'EA Panamá - Editor', image: 'imagenes/logoRedes.jpg', url: '/admin/titulo'})
 })
 
 module.exports = admin
--- app.js
+++ app.js
@@ -122,7 +122,10 @@ app.post('/contactar/send', function (req, res, next) {
   });
 })
 app.get('/404', function(reg, res){
-  res.render('index.pug', {image: 'imagenes/logoRedes.jpg'})
+  res.render('index.pug', {
+    image: 'imagenes/logoRedes.jpg',
+    url: '/404'
+  })
 })
 
 app.use(function(reg, res){
