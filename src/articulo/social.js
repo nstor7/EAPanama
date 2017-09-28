e8ddd3e8754b82e007e4346f789a2b1419528bcd
Néstor Ibarra <nstor7@hotmail.com>
2017-09-27 19:35:26 -0500

    cambio de social

--- src/articulo/social.js
+++ src/articulo/social.js
@@ -4,9 +4,9 @@ import social from './socialFunctions'
 module.exports = function(articulo){
   var el = yo`
   <div class="social" id="social">
-    <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://sumergidapro.com/blog/${articulo.titulo.replace(/ /g, '-')}"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-    <a target="_blank" href="http://twitter.com/home?status=http://sumergidapro.com/blog/${articulo.titulo.replace(/ /g, '-')}"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-    <a href="https://plus.google.com/share?url=https://sumergidapro.com/blog/${articulo.titulo.replace(/ /g, '-')}" onclick="javascript:window.open(this.href,
+    <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://eapanama.com/blog/${articulo.titulo.replace(/ /g, '-')}"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+    <a target="_blank" href="http://twitter.com/home?status=http://eapanama.com/blog/${articulo.titulo.replace(/ /g, '-')}"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+    <a href="https://plus.google.com/share?url=https://eapanama.com/blog/${articulo.titulo.replace(/ /g, '-')}" onclick="javascript:window.open(this.href,
     '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;"><i class="fa fa-google-plus" aria-hidden="true"></i></a>  
   </div>
   `
