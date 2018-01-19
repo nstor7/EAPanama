2bc11e3814b599f94f9cd2b1d4e6aa15bb9a195f
Pablo <pablo.iglesias@eapanama.com>
2018-01-19 12:19:35 -0500

    sdas

--- src/productos/tarjeta.js
+++ src/productos/tarjeta.js
@@ -3,7 +3,7 @@ import yo from 'yo-yo'
 module.exports = function(producto){
   var el = yo`
     <a class="productoTarjeta" style="background: url('${producto.interna}'); background-size: contain; background-repeat: no-repeat" target="_blank">
-      <h4 class="productoTarjeta">${producto.titulo}</h4>
+      <h4>${producto.titulo}</h4>
       <div class="productoOver">
         <div class="productoOverImagen" style="background: url('${producto.externa}'); background-size: contain; background-repeat: no-repeat"></div>
         <p>${producto.descripcion}</p>
