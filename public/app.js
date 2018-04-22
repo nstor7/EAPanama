bf8d926ba9fabb52089ba5a9423b49ce7c758019
Pablo <pablo.iglesias@eapanama.com>
2018-04-22 10:08:12 -0500

    adding keywords

--- public/app.js
+++ public/app.js
@@ -14120,8 +14120,8 @@ function _interopRequireDefault(obj) {
 'use strict';
 
 module.exports = {
-  title: 'EAPanamá | Ingenieros Acústicos',
-  keywords: 'Acondicionamiento acústico, Aislamiento acústico, Control de ruido industrial, Instalaciones de audio, Venta de materiales acústicos.',
+  title: 'EAPanamá | Consultores Acústicos',
+  keywords: 'Acondicionamiento acústico, Aislamiento acústico, Control de ruido industrial, Instalaciones de audio, Venta de materiales acústicos, ruido, acústica, eco, reverberación, aislante térmico, aislante termoacústico',
   description: 'En EA Panamá ofrecemos todo tipo de soluciones acústicas de calidad en los campos de aislamiento en la edificación, acústica medioambiental, diseño y acondicionamiento de recintos, instalaciones audiovisuales así como venta de materiales acústicos, sonógrafos y equipos de audio.' };
 
 },{}],76:[function(require,module,exports){
